5697f049ebd2e128ffa69c5e74c75923668197b9
Kinetics20 <kinetics20@gmail.com>
2026-03-09 22:45:26 +0100

    feat(cli): read and save to file

--- src/bootstrap.py
+++ src/bootstrap.py
@@ -2,4 +2,7 @@ from src.todo_list.todo_list import TodoList
 
 
 def bootstrap_tasks() -> TodoList:
-    return TodoList()
+    with open('./temp/list_task.json', encoding='utf8') as file:
+        content = file.read()
+
+    return TodoList.from_json(content)
